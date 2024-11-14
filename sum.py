974d45e68a301b469ca8e09cef0e46aa72cb6f4b
Gouravsen1202 <gouravsen757@gmail.com>
2024-11-14 11:12:20 +0530

    first commit

---
+++ sum.py
@@ -0,0 +1,107 @@
+'''a=int(input('entr 1 no'))
+b=int(input("entr 2 no"))
+def hi():
+    print(a+b)
+if(a>=b):
+    print('true')
+else:
+    print('false')'''
+'''a=int(input('enter u no'))
+b=1
+sum=0
+while(b<=a):
+    sum=sum+(b*b)
+    b=b+1
+    print('sum=',sum)'''
+'''a=int(input('enter u no'))
+b=1
+while(b<=a):
+    print(b)
+    b=b+1'''
+'''a=int(input('entr u no'))
+b=1
+sum=0
+while(a<=b):
+    print(a)
+    sum=sum*a
+    print(sum)'''
+'''a=int(input('enter u no'))
+b=1
+sum=0
+while(b<=a):
+    sum=sum+(a*a)
+    b=b+1
+print(sum)'''
+'''a=int(input('enter u no'))
+b=1
+while(b<=a):
+    print(b)
+    b=b+2'''
+'''a=int(input('enter u no'))
+b=2
+sum=0
+while(a>=b):
+    sum=sum+b
+    b=b+2
+    print(sum)'''
+'''a=int(input('entr u no'))
+b=int(input('entr 2 no'))
+ope=(input('enter u operator'))
+if ope == "+":
+    print(a+b)
+elif ope =="-":
+    print(a-b)
+elif ope =="*":
+    print(a*b)
+elif ope =="/":
+    print(a/b)
+else:
+    print('please selecr oparatpr')  
+a= int(input('enter u bill unit'))
+b=a*5
+c=b-(10):
+print(c,'rupeas')''' 
+
+
+'''10th class marksheeet calculter
+h=(input('enter your name = '))
+a=eval(input('enter u hindi no'))
+b=eval(input('enter u  english no'))
+c=eval(input('enter u maths no'))
+d=eval(input('enter u  sanskrit no'))
+e=eval(input('enter u  sst no'))
+
+sum=(a+b+c+d+e)
+f=sum/5
+
+print("your name is:",h)
+if sum>=75:
+    print(h,' u are pass in first division')
+elif sum>=45:
+     print(h,' u are pass in second division')
+elif sum>=33:
+      print(h,' u are pass in third division')
+else:
+     print('fail.... better luck next year')
+print('your persentage is',f,(sum))'''
+
+
+'''electricity  bill'''
+'''def hero():
+ a=eval(input('enter your electric unit'))
+ b=a*5
+ c=(b/100)*10
+ d=b-c
+ print(d)'''
+
+''' profit of 5% on our sale'''
+''''a=eval(input('enter u 1st product profit'))
+b=eval(input('enter u 2st product profit'))
+c=a+b
+d=(c/100)*5
+print(d)'''
+
+
+
+
+
